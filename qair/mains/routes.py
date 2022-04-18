c0943296a3354cc42c85bc88cb96aff0a003821e
Ashiq666 <mn.ashiq07@gmail.com>
2022-04-18 16:26:50 +0600

    Implmented explore and it's sub htmt, routes etc.

--- qair/mains/routes.py
+++ qair/mains/routes.py
@@ -11,6 +11,30 @@ def homepage():
     return render_template("mains/homepage.html")
 
 @mains.route("/explore")
-@mains.route("/")
 def explore():
     return render_template("mains/explore.html")
+
+@mains.route("/card-1")
+def card_1():
+    return render_template("mains/card-1.html")
+
+@mains.route("/card-2")
+def card_2():
+    return render_template("mains/card-2.html")
+
+@mains.route("/card-3")
+def card_3():
+    return render_template("mains/card-3.html")
+
+@mains.route("/card-4")
+def card_4():
+    return render_template("mains/card-4.html")
+
+@mains.route("/card-5")
+def card_5():
+    return render_template("mains/card-5.html")
+
+@mains.route("/card-6")
+def card_6():
+    return render_template("mains/card-6.html")
+
