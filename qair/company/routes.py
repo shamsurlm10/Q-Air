283b950762349414a09971ba580f4e98c7293d87
shamsurlm10 <shamsur.works@gmail.com>
2022-04-24 16:38:02 +0600

    Implemented add plane and edit plane

--- qair/company/routes.py
+++ qair/company/routes.py
@@ -1,17 +1,18 @@
 import re
+
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
-from qair.company.utils import remove_photo, save_photos
-from qair.company.forms import *
 from qair import bcrypt, db
-from qair.models import Company, Route
+from qair.company.forms import *
+from qair.company.utils import remove_photo, save_photos
+from qair.models import Airplane, Company, Route
 
 company = Blueprint("company", __name__, url_prefix="/company")
 
 
 @company.route("/view-company/<int:id>")
 @login_required
-def view_company(id:int):
+def view_company(id: int):
     company = Company.query.get(id)
     return render_template("company/view-company.html", company=company)
 
@@ -30,7 +31,6 @@ def create_company():
         flash(f"Company created", "success")
         return redirect(url_for("company.add_planes"))
     return render_template("company/create-company.html", form=form)
-
 
 
 @company.route("/edit-company", methods=["POST", "GET"])
@@ -70,6 +70,7 @@ def edit_company():
         form.company_name.data = company.company_name
     return render_template("company/edit-company.html", form=form, company=company)
 
+
 @company.route("/remove-profile-photo")
 @login_required
 def remove_profile_photo():
@@ -82,6 +83,7 @@ def remove_profile_photo():
         flash("Cannot remove default profile photo.", "danger")
     return redirect(url_for("company.edit_company"))
 
+
 @company.route("/settings/remove-cover-photo")
 @login_required
 def remove_cover_photo():
@@ -93,6 +95,7 @@ def remove_cover_photo():
     else:
         flash("Cannot remove default cover photo.", "danger")
     return redirect(url_for("company.edit_company"))
+
 
 @company.route("/create-flight", methods=["POST", "GET"])
 @login_required
@@ -110,18 +113,52 @@ def edit_flight():
     form = EditFlight()
     airplanes = current_user.profile.company.airplanes
     routes = current_user.profile.company.routes
-    return render_template("company/edit-flight.html", form=form, airplanes=airplanes, len=len, routes=routes)
+    return render_template("company/edit-flight.html", form=form,  airplanes=airplanes, len=len, routes=routes)
+
 
 @company.route("/add-planes", methods=["POST", "GET"])
 @login_required
 def add_planes():
     form = AddPlanes()
+    if form.validate_on_submit():
+        if not current_user.profile.company:
+            flash('No company found!', 'danger')
+            return redirect(url_for('company.add_planes'))
+        airplane = Airplane(form.airplane_name.data,
+                            form.airplane_model.data, current_user.profile.company.id,
+                            form.passenger_capacity.data)
+        db.session.add(airplane)
+        db.session.commit()
+        flash('Plane created.', 'success')
+        return redirect(url_for("company.add_planes"))
     return render_template("company/add-planes.html", form=form)
 
-@company.route("/edit-planes", methods=["POST", "GET"])
+
+@company.route("/view-planes", methods=["POST", "GET"])
 @login_required
-def edit_planes():
+def view_planes():
+    planes = Airplane.query.all()
+    return render_template("company/view-planes.html", planes=planes)
+
+
+@company.route("/edit-planes/<int:id>", methods=["POST", "GET"])
+@login_required
+def edit_planes(id: int):
     form = EditPlanes()
+    plane = Airplane.query.get(id)
+    if not plane:
+        flash('Plane not found!', 'danger')
+        return redirect(url_for('company.view_planes'))
+    if form.validate_on_submit():
+        plane.airplane_name = form.airplane_name.data
+        plane.airplane_model = form.airplane_model.data
+        plane.passenger_capacity = form.passenger_capacity.data
+        db.session.commit()
+        flash('Plane updated successfully.', 'success')
+        return redirect(url_for('company.edit_planes', id=plane.id))
+    form.airplane_name.data = plane.airplane_name
+    form.airplane_model.data = plane.airplane_model
+    form.passenger_capacity.data = plane.passenger_capacity
     return render_template("company/edit-planes.html", form=form)
 
 
@@ -130,7 +167,11 @@ def edit_planes():
 def create_route():
     form = CreateRoute()
     if form.validate_on_submit():
-        route = Route(form.origin.data, form.destination.data, form.distance.data, form.duration.data, current_user.profile.company.id)
+        if not current_user.profile.company:
+            flash('No company found!', 'danger')
+            return redirect(url_for('company.create_route'))
+        route = Route(form.origin.data, form.destination.data, form.distance.data,
+                      form.duration.data, current_user.profile.company.id)
         db.session.add(route)
         db.session.commit()
         flash("Route Created.", "success")
