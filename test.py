a18de3349ff4575ccbd7e549124d10919fba1ab5
shamsur rahman <shamsur.works@gmail.com>
2022-05-08 06:02:11 +0600

    feat: Small commit for unit test 2nd time

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from qair import app
 import unittest
 
+from qair.reservations.routes import payment
+
 class QairTestCase(unittest.TestCase):
     def test_login(self):
         tester = app.test_client(self)
@@ -16,12 +18,27 @@ class QairTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/users/forget_password', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+    
+    def test_view_flights(self):
+        tester = app.test_client(self)
+        response = tester.get('/company/view-flight', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
         
-    def test_base_route(self):
-        client = app.test_client(self)
-        url = '/admins/dashboard'
-        response = client.get(url)
-        assert response.status_code == 200
+    def test_create_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/company/create-route', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+    
+    def test_change_photos(self):
+        tester = app.test_client(self)
+        response = tester.get('/profiles/change-photos', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+        
+    def test_notifications(self):
+        tester = app.test_client(self)
+        response = tester.get('/notifications/notifications', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
+        
 
 if __name__ == "__main__":
     unittest.main()
